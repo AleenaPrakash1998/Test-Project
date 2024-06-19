ff8c91bca323aceed048278328daaea7f3b73553
Aleena_Prakash <aleena@pixbitsolutions.com>
2024-06-19 12:18:30 +0000

    fix:modal repeated content removed

--- resources/views/pages/entities/modal/success-modal.blade.php
+++ resources/views/pages/entities/modal/success-modal.blade.php
@@ -2,11 +2,10 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="successModalLabel">Successfully updated entity!</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                Entity updated successfully.
+                <h4 class="modal-title" id="successModalLabel">Successfully updated entity!</h4>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
